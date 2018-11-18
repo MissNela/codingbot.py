a51eb776bf084a279eb570de19ed748e753066ac
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-18 13:29:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,8 @@ async def on_member_join(member):
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
     embed.set_author(name='Welcome message')
-    embed.add_field(name = '__Welcome to Our Server__',value ='''**Hope you will be active here. Rules: No raiding
+    embed.add_field(name = '__Welcome to Our Server__',value ='''**Hope you will be active here. Rules:
+**No raiding
 No dangerous/IP booter code
 No asking for admin
 No abusing ranks
@@ -38,12 +39,12 @@ No racism
 Do not insult other people
 Do not start fights
 Do. Not ask for admin unless you are applying
-No staff disrespect this will result in a ban  ''',inline = False)
+No staff disrespect this will result in a ban**  ''',inline = False)
     embed.set_image(url = 'https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif')
 
     await client.send_message(member,embed=embed)
     print("Sent message to " + member.name)
-    channel = discord.utils.get(client.get_all_channels(), server__name='DarkBot Official Server', name='darkbot-servers-join-leave-log')
+    channel = discord.utils.get(client.get_all_channels(), server__name='Bot Support Central', name='welcome')
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     embed = discord.Embed(title=f'Welcome {member.name} to {member.server.name}', description='''Do not forget to read our rules @here  is the rules
 No raiding

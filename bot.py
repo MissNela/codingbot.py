b2448dc00457dd79c65a190e895f60c58837bb1a
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-18 21:21:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -132,5 +132,16 @@ async def warn(ctx, userName: discord.User, *, message:str):
         await client.say("warning {0} Has Been Warned! Warning Reason : {1} ".format(userName,message))
         pass
     
- 
+ @client.command(pass_context=True, aliases=['em', 'e'])
+async def modmail(ctx, *, msg=None):
+    channel = discord.utils.get(client.get_all_channels(), name='logs')
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    color = discord.Color((r << 16) + (g << 8) + b)
+    if not msg:
+        await client.say("Please specify a message to send")
+    else:
+        await client.send_message(channel, embed=discord.Embed(color=color, description=msg + '\n Message From-' + ctx.message.author.id))
+        await client.delete_message(ctx.message)
+    return
+
 client.run(os.getenv("BOT_TOKEN"))

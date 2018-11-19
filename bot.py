a6e50544becad05972bedf859162a072e80f93ab
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-19 19:06:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from discord import Game, Embed, Color, Status, ChannelType
 
 
 
-startup_extensions = ["Music"]
+
 client = commands.Bot(command_prefix = '(')
 
 client.remove_command('help')
@@ -66,10 +66,7 @@ No staff disrespect this will result in a ban and never try to break any one of 
     embed.set_thumbnail(url=member.avatar_url)
     await client.send_message(channel, embed=embed)
     
-class Main_Commands():
-        def __int__(self, bot):
-         self.bot = bot
-   
+
 
 @client.command(pass_context=True)
 async def noncoder(ctx):
@@ -237,13 +234,33 @@ async def unmute(ctx, user: discord.Member):
     await client.remove_roles(user, role)
     await client.delete_message(ctx.message)
 
-if __name__ = "__main__":
-    for extension in startup_extension:
-        try:
-            bot.load_extension(extension)
-        expekt Exception as e:
-            exc = '(): ()'.format(type(e).__name__, e)
-            print('Failed to load extension ()\n()'.format(extension, exc))
-                       
+
+@client.command(pass_context=True)
+async def chat1(ctx, *, msg=None):
+
+
+    channel = discord.utils.get(client.get_all_channels(), name='bot-chat1')
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    color = discord.Color((r << 16) + (g << 8) + b)
+    if not msg:
+        await client.say("Please specify a message to send")
+    else:
+        await client.send_message(channel, embed=discord.Embed(color=color, description=msg + '\n Message From-' + ctx.message.author.id))
+        await client.delete_message(ctx.message)
+    return
+
+client.command(pass_context=True)
+async def chat2(ctx, *, msg=None):
+
+
+    channel = discord.utils.get(client.get_all_channels(), name='bot-chat2')
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    color = discord.Color((r << 16) + (g << 8) + b)
+    if not msg:
+        await client.say("Please specify a message to send")
+    else:
+        await client.send_message(channel, embed=discord.Embed(color=color, description=msg + '\n Message From-' + ctx.message.author.id))
+        await client.delete_message(ctx.message)
+    return
 
 client.run(os.getenv("BOT_TOKEN"))

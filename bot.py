bf7ec63e1013eb3f8f40e87f75b94a5c65ec9b52
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-26 21:53:05 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -221,16 +221,15 @@ async def modmail(ctx, *, msg=None):
     if not msg:
         await client.say("Please specify a message to send")
     else:
-        await client.send_message(channel, embed=discord.Embed(color=color, description=msg + '\n Message From-' + ctx.message.author.id))
+        await client.send_message(channel, embed=discord.Embed(color=color, description=msg '\n Message From-' + ctx.message.author.id))
         await client.delete_message(ctx.message)
     return
 
 @client.command(pass_context=True)
-@commands.has_permissions(manage_roles = True)
 async def suggest(ctx, *, msg=None):
 
 
-    channel = discord.utils.get(client.get_all_channels(), name='announcements')
+    channel = discord.utils.get(client.get_all_channels(), name='🎀logs-1🎀)
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     color = discord.Color((r << 16) + (g << 8) + b)
     if not msg:
@@ -241,16 +240,17 @@ async def suggest(ctx, *, msg=None):
     return
 
 @client.command(pass_context=True)
+@commands.has_permissions(manage_roles = True)
 async def announce(ctx, *, msg=None):
 
 
-    channel = discord.utils.get(client.get_all_channels(), name='suggestions')
+    channel = discord.utils.get(client.get_all_channels(), name='🎀announcements🎀')
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     color = discord.Color((r << 16) + (g << 8) + b)
     if not msg:
         await client.say("Please specify a message to send")
     else:
-        await client.send_message(channel, embed=discord.Embed(color=color, description=msg + '\n Message From-' + ctx.message.author.id))
+        await client.send_message(channel, embed=discord.Embed(color=color, description=msg + '\n Message From-' + ctx.message.author))
         await client.delete_message(ctx.message)
     return
 
